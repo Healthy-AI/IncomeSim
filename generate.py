88c453153dfa0f0e1e3cd00f5396b4292a994e2a
Fredrik Johansson <ml@fredjo.com>
2023-06-09 14:32:04 +0200

    updated output format

--- generate.py
+++ generate.py
@@ -44,7 +44,33 @@ if __name__ == "__main__":
     np.random.seed(args.seed)
     print('Sampling observations ...')
     S = A.sample(args.n_samples, T=args.horizon)
+   
+    # Prep data
+    df0 = S[S['time']==0]
+    df = df0.copy().rename(columns={'income': 'income_current'})
+
+    Tend = args.horizon-1
+    df['income'] = S[S['time']==Tend]['income'].values
+
+    df.loc[df['training']=='No training','training'] = 'No studies'
     
+    # Make categorical columns have the right type
+    c_cols = ['native-country', 'sex', 'race', 'education', 
+              'training', 'workclass', 'occupation', 'marital-status', 'relationship']
+    df[c_cols] = df[c_cols].astype('category')
+
+    # Drop index columns
+    df = df.drop(columns=['time','id'])
+    
+    # Reorder columns
+    special_cols = ['training', 'income']
+    df = df[[c for c in df.columns if c not in special_cols] + special_cols]
+    
+    # Rename columns
+    df = df.rename(columns={'training': 'studies'})
+    
+    # Save data to file
     fname = '%s_m%d_T%d_s%d.pkl' % (args.label, args.n_samples, args.horizon, args.seed)
-    S.to_pickle('data/%s' % fname)
+    df.to_pickle('data/%s' % fname)
     print('Saved result to: %s' % fname)
+    
