39527072cb0a54a4410d6b5c76348901b044f438
Fredrik Johansson <ml@fredjo.com>
2024-05-04 07:21:33 +0200

    starting to build estiamtors

---
+++ income/estimators.py
@@ -0,0 +1,5 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import roc_auc_score
+from sklearn.evaluation import GridSearchCV
+
